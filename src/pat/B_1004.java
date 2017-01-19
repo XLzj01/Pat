7ada7d8608b91d359d48eb489ad20eb154bd203b
XLzj01 <616774169@qq.com>
2017-01-19 21:57:35 +0800

    finish B_1004

--- src/pat/B_1004.java
+++ src/pat/B_1004.java
@@ -1,6 +1,5 @@
 package pat;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class B_1004 {
@@ -8,7 +7,8 @@ public class B_1004 {
 		StringBuffer rightDay = new StringBuffer();
 		Scanner scanner = new Scanner(System.in);
 		String[] strings = new String[4];
-
+		String[] days = { "MON ", "TUE ", "WED ", "THU ", "FRI ", "SAT ", "SUN " };
+		int[] hours = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, };
 		for (int i = 0; i < strings.length; i++) {
 			String temp = scanner.nextLine();
 			strings[i] = temp;
@@ -17,45 +17,35 @@ public class B_1004 {
 		char[] dayCharArray = strings[0].toCharArray();
 		String dayString = strings[1];
 		int count = 1;
-
 		for (int i = 0; i < dayCharArray.length; i++) {
 			Character subtitle = dayCharArray[i];
 			int dayCharArrayNumble = subtitle;
-			if (65 < dayCharArrayNumble && 90 > dayCharArrayNumble) {
-				int indexOf = dayString.indexOf(subtitle.toString());
-				Character compareDayString = dayString.charAt(indexOf);
-				if (i == indexOf && count == 1 && subtitle == compareDayString) {
-					count++;
-					switch (subtitle.toString()) {
-					case "A":
-						rightDay.append("MON ");
-						break;
-					case "B":
-						rightDay.append("TUE ");
-						break;
-					case "C":
-						rightDay.append("WED ");
-						break;
-					case "D":
-						rightDay.append("THU ");
-						break;
-					case "E":
-						rightDay.append("FRI ");
-						break;
-					case "F":
-						rightDay.append("SAT ");
-						break;
-					case "G":
-						rightDay.append("SUN ");
-						break;
-
-					}
-				}
-				if (i == indexOf && count == 1 && subtitle == compareDayString) {
-					rightDay.append("SAT ");
-					
+			if (65 <= dayCharArrayNumble && 71 >= dayCharArrayNumble && count == 1
+					&& i == dayString.indexOf(subtitle.toString())) {
+				rightDay.append(days[dayCharArrayNumble - 65]);
+				count++;
+			} else if (count == 2 && i == dayString.indexOf(subtitle.toString())) {
+				if (48 <= dayCharArrayNumble && 57 >= dayCharArrayNumble) {
+					rightDay.append(hours[dayCharArrayNumble - 48]).append(":");
+				} else if (65 <= dayCharArrayNumble && 78 >= dayCharArrayNumble) {
+					rightDay.append(hours[dayCharArrayNumble - 65 + 10]).append(":");
 				}
 			}
 		}
+
+		char[] timeCharArray = strings[2].toCharArray();
+		for (int i = 0; i < timeCharArray.length; i++) {
+			Character subtitle = timeCharArray[i];
+			int dayCharArrayNumble = subtitle;
+			if (97 <= dayCharArrayNumble && 122 >= dayCharArrayNumble && i == strings[3].indexOf(subtitle.toString())) {
+				if (i < 10) {
+					rightDay.append(0).append(i);
+				} else {
+					rightDay.append(i);
+				}
+
+			}
+		}
+		System.out.println(rightDay.toString());
 	}
 }
