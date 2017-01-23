972816070603f98865fef93e6d26956d26f0244d
XLzj01 <616774169@qq.com>
2017-01-23 22:41:30 +0800

    finish B_1006

---
+++ src/pat/B_1006.java
@@ -0,0 +1,30 @@
+package pat;
+
+import java.util.Scanner;
+
+public class B_1006 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String tempString = scanner.nextLine();
+		String[] tempArray = tempString.split("\\ ");
+		String a = tempArray[0];
+		String b = tempArray[1];
+		String c = tempArray[2];
+		String d = tempArray[3];
+
+		int numble1 = getNumble(a, b);
+		int numble2 = getNumble(c, d);
+		System.out.println(numble1 + numble2);
+	}
+
+	public static int getNumble(String a, String b) {
+		StringBuffer count = new StringBuffer();
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) == b.charAt(0)) {
+				count.append(a.charAt(i));
+			}
+		}
+		Integer numble = new Integer(count.toString());
+		return numble;
+	}
+}
