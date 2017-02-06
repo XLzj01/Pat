f2ad4eac8d6ce9639ba76fd8c79285f4cf950993
XLzj01 <616774169@qq.com>
2017-02-06 22:16:44 +0800

    finish B_1007

---
+++ src/pat/B_1007.java
@@ -0,0 +1,13 @@
+package pat;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B_1007 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		int b = sc.nextInt();
+		System.out.println(a.divide(BigInteger.valueOf(b)) + " " + a.mod(BigInteger.valueOf(b)));
+	}
+}
