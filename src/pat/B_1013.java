c60e143b85188f26ae87b3a229e61e8b57274e03
XLzj01 <616774169@qq.com>
2017-02-15 22:17:39 +0800

    finish B_1013

---
+++ src/pat/B_1013.java
@@ -0,0 +1,26 @@
+package pat;
+
+import java.util.Scanner;
+
+public class B_1013 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String nextLine = scanner.nextLine();
+		String[] split = nextLine.split("\\ ");
+		StringBuffer buffer = new StringBuffer();
+		int count = 0;
+		for (int i = 0; i < split.length; i++) {
+			int a = new Integer(split[i]);
+			for (int j = 0; j < a; j++) {
+				if (count == 0 && i != 0) {
+					buffer.insert(0, i);
+					count++;
+					continue;
+				}
+				buffer.append(i);
+			}
+		}
+		System.out.println(buffer);
+		scanner.close();
+	}
+}
